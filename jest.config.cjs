18264407ad14cd12a9fd5ac1c3ca7bca0459f222
mhmdrazn <razan4944@gmail.com>
2025-11-29 17:28:04 +0700

    chore: remove duplicate jest.config.cjs

--- jest.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-};
